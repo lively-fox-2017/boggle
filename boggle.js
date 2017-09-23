ecd38418253c093ced165f587df9f73410c0607a
Ian Max <ianmax1983@gmail.com>
2017-09-23 21:05:16 +0700

    boggle

---
+++ boggle.js
@@ -0,0 +1,22 @@
+class Boggle {
+  constructor() {
+    this.arr = [];
+  }
+
+  shake(number) {
+    let words = 'abcdefghijklmnopqrstuvwxyz';
+    for (let x = 0; x < number; x++) {
+      this.arr[x] = [];
+      for (let y = 0; y < number; y++) {
+        this.arr[x][y] = (words.toUpperCase()[Math.floor(Math.random() * words.length)]);
+      }
+    }
+
+    return this.arr;
+  }
+}
+
+let boggleGame = new Boggle();
+
+// boggleGame.shake(4);
+console.log(boggleGame.shake(4));
