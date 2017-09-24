2c906257c73699d27873e272dc6f0c578fbf4a17
setiadijoe <setiadi_y56@yahoo.co.id>
2017-09-24 21:49:59 +0700

    pengecekkan indeks tetangga terdekat

--- boggle.js
+++ boggle.js
@@ -1,6 +1,7 @@
 class Boggle {
     constructor() {
         this.board = [];
+        this.history = [];
     }
 
     randomAlph() {
@@ -22,10 +23,26 @@ class Boggle {
         this.board = arr
         // console.log(this.board)
         // return this.board;
-        return this.board
+        return [['Z', 'B', 'O', 'Q'], ['A', 'C', 'E', 'Y'], ['K', 'L', 'U', 'B'], ['V', 'A', 'T', 'X']]
+    }
+
+    cekIndex(idx1, idx2) {
+        if (Math.abs(idx1[0] - idx2[0]) === 1 || Math.abs(idx1[1] - idx2[1]) === 1) {
+            return true
+        } else {
+            return false
+        }
+    }
+
+    cekKata() {
+        // perlu membuat yang namanya history
     }
 }
 
 let huruf = new Boggle
 
 console.log(huruf.shake(4))
+console.log(huruf.cekIndex([1,1], [1,1]))
+console.log(huruf.cekIndex([1, 1], [1, 2]))
+console.log(huruf.cekIndex([1, 2], [1, 3]))
+console.log(huruf.cekIndex([0, 1], [1, 2]))
