86a83d1e7134530207908bc123fccc77eb69b3d9
setiadijoe <setiadi_y56@yahoo.co.id>
2017-09-24 21:33:05 +0700

    berhasil bikin boardnya

--- boggle.js
+++ boggle.js
@@ -4,12 +4,28 @@ class Boggle {
     }
 
     randomAlph() {
-        // let text = '';
+        // ini fungsi untuk memberikan huruf secara random
+        // jumlah hurufnya satu
         let alphabet = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
         return alphabet.charAt(Math.floor(Math.random() * alphabet.length));
+    }
+
+    shake(num) {
+        let arr = [];
+        for (let i = 0; i < num; i++) {
+            let temp = [];
+            for (let j = 0; j < num; j++) {
+                temp.push(this.randomAlph());
+            }
+            arr.push(temp);
+        }
+        this.board = arr
+        // console.log(this.board)
+        // return this.board;
+        return this.board
     }
 }
 
 let huruf = new Boggle
 
-console.log(huruf.randomAlph())
+console.log(huruf.shake(4))
